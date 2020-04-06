a8e740710b10160389e0c6d778238a4a0a7a68af
marcin007 <marcin007wojciechowski@gmail.com>
2020-04-06 20:31:12 +0200

    Deleted Channel Query

--- src/main/java/com/marcinwo/youtubeapi/demo/repository/ChannelRepository.java
+++ src/main/java/com/marcinwo/youtubeapi/demo/repository/ChannelRepository.java
@@ -16,15 +16,6 @@ public interface ChannelRepository extends JpaRepository<Channel, Long> {
     @Query(value = "select a.channels From User a where a.id = ?1" )
     List<Channel> findAllByUser_Id(Long id);
 
-//    @Modifying
-//    @Query(value = "INSERT INTO channels(name, description,user_id) values(:name, :description, :id)", nativeQuery = true)
-//    Channel postUserChannelById(@Param("name") Channel channel.name, Channel channel.description);
-
-    @Query(value = "INSERT INTO channels(user_id, name, description,) values(?1, ?2, ?3)")
-    Channel postUserChannelById(Long id, Channel channel);
-
-                                            // channel.name -> do parametru 2
-                                            // channel.description > do parametru 3
 
 
 }
