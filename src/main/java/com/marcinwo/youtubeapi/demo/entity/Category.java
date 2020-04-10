76c19e8b46832733dc0e3f1967c006bf5a461ce3
marcin007 <marcin007wojciechowski@gmail.com>
2020-04-10 20:43:15 +0200

    fix for Category

--- src/main/java/com/marcinwo/youtubeapi/demo/entity/Category.java
+++ src/main/java/com/marcinwo/youtubeapi/demo/entity/Category.java
@@ -1,4 +1,18 @@
 package com.marcinwo.youtubeapi.demo.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "categories")
+@Entity
 public class Category {
 }
