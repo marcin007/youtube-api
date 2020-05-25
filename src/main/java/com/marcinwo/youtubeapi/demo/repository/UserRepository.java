c5895cb4ccbec0b1a9867e52b332f2e3478da87c
marcin007 <marcin007wojciechowski@gmail.com>
2020-05-25 14:34:34 +0200

    Added UserRepositoryTest init.

--- src/main/java/com/marcinwo/youtubeapi/demo/repository/UserRepository.java
+++ src/main/java/com/marcinwo/youtubeapi/demo/repository/UserRepository.java
@@ -4,7 +4,14 @@ import com.marcinwo.youtubeapi.demo.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUserName(String username);
+
+    List<User> findAllByLastName(String lastName);
 
 }
---
+++ src/test/java/com/marcinwo/youtubeapi/demo/repository/UserRepositoryTest.java
@@ -0,0 +1,86 @@
+package com.marcinwo.youtubeapi.demo.repository;
+
+import com.marcinwo.youtubeapi.demo.entity.User;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class UserRepositoryTest {
+
+    @Autowired
+    private TestEntityManager entityManager;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    // given - wejscie
+    // when - zachowanie
+    // then - oczekiwane wyjscie
+
+    @Before
+    public void beforeEach() {
+        userRepository.deleteAll();
+    }
+
+    @Test
+    public void whenFindByUserName_thenReturnUser() {
+        // given
+        User user = new User("Jacek", "Adamowski", "jadamowski", "1234",
+                new HashSet<>(), new HashSet<>(), new HashSet<>());
+        entityManager.persist(user);
+        entityManager.flush();
+
+        // when
+        String username = "jadamowski";
+        Optional<User> optionalUser = userRepository.findByUserName(username);
+
+        // then
+        assertThat(optionalUser.isPresent()).isTrue();
+        assertThat(optionalUser.get().getLastName()).isEqualTo(user.getLastName());
+    }
+
+    @Test
+    public void whenFindAllByLastName_thenReturnUsers() {
+        User user1 = new User("Jacek1", "Adamowski1", "jadamowski1", "1234",
+                new HashSet<>(), new HashSet<>(), new HashSet<>());
+        User user2 = new User("Jacek2", "Adamowski1", "jadamowski2", "1234",
+                new HashSet<>(), new HashSet<>(), new HashSet<>());
+
+        entityManager.persist(user1);
+        entityManager.persist(user2);
+        entityManager.flush();
+
+        String lastname = "Adamowski1";
+        List<User> users = userRepository.findAllByLastName(lastname);
+
+
+        assertThat(users.size() == 2).isTrue();
+        assertThat(users.get(0).getFirstName()).isEqualTo(user1.getFirstName());
+        assertThat(users.get(1).getFirstName()).isEqualTo(user2.getFirstName());
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void testException() {
+        // given
+
+        // when
+
+        //przyklad
+        // userService.findByUsername("ibwifyeb").getName();
+        throw new RuntimeException();
+    }
+
+
+}
