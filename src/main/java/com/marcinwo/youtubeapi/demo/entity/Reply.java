c53795714d7bdf912331ad5a352f287d07b53bc7
marcin007 <marcin007wojciechowski@gmail.com>
2020-06-05 20:46:00 +0200

    Added changes to Reply.

--- src/main/java/com/marcinwo/youtubeapi/demo/entity/Reply.java
+++ src/main/java/com/marcinwo/youtubeapi/demo/entity/Reply.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
@@ -18,12 +19,12 @@ import java.time.LocalDateTime;
 public class Reply extends AbstractEntity {
 
 
-    @ManyToOne
+    @ManyToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
 
-    @ManyToOne
+    @ManyToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "comment_id", nullable = false)
     private Comment comment;
 
--- src/main/java/com/marcinwo/youtubeapi/demo/repository/ReplyRepository.java
+++ src/main/java/com/marcinwo/youtubeapi/demo/repository/ReplyRepository.java
@@ -5,9 +5,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ReplyRepository extends JpaRepository<Reply, Long> {
 
     List<Reply> findAllByCommentId(Long id);
+    Optional<Reply> findByUserId(Long id);
+    List<Reply> findAllByUser_UserName(String s);
 }
