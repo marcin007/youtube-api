4f3819e6856b168078292d7e9d7b2b9eb131cd84
marcin007 <marcin007wojciechowski@gmail.com>
2020-06-08 17:25:06 +0200

    Added ApiError init.

---
+++ src/main/java/com/marcinwo/youtubeapi/demo/exeption/ApiError.java
@@ -0,0 +1,15 @@
+package com.marcinwo.youtubeapi.demo.exeption;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ApiError {
+    private String error;
+    private int code;
+}
