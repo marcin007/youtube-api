e98e2c8abc86d021f80f040fa89f7af516b4ac3a
marcin007 <marcin007wojciechowski@gmail.com>
2020-06-08 17:25:29 +0200

    Added CustomExceptionHandler init.

---
+++ src/main/java/com/marcinwo/youtubeapi/demo/exeption/CustomExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.marcinwo.youtubeapi.demo.exeption;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
+
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<ApiError> handlerUserNotFoundException (UserNotFoundException e, WebRequest webRequest){
+        return new ResponseEntity<>(new ApiError("User not found.", HttpStatus.NOT_FOUND.value()), HttpStatus.NOT_FOUND);
+    }
+
+
+
+
+
+}
