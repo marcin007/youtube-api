6fe4ba10b8371797bc9ff4f75bfc72e83d55709b
marcin007 <marcin007wojciechowski@gmail.com>
2020-06-08 17:27:15 +0200

    Added improvements to CommentMapperTestIT.

--- src/test/java/com/marcinwo/youtubeapi/demo/mapper/CommentMapperTestIT.java
+++ src/test/java/com/marcinwo/youtubeapi/demo/mapper/CommentMapperTestIT.java
@@ -24,7 +24,7 @@ import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class CommentMapperTest {
+public class CommentMapperTestIT {
 
     @Autowired
     private UserRepository userRepository;
