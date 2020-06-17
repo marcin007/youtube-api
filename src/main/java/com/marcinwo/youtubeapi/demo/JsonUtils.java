e8d47320d92a1e784590e41e934d943d4e3c765b
marcin007 <marcin007wojciechowski@gmail.com>
2020-06-17 12:54:42 +0200

    Added fixes parsing LocalDateTime.

--- src/main/java/com/marcinwo/youtubeapi/demo/JsonUtils.java
+++ src/main/java/com/marcinwo/youtubeapi/demo/JsonUtils.java
@@ -2,6 +2,8 @@ package com.marcinwo.youtubeapi.demo;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 public class JsonUtils {
 
@@ -9,6 +11,9 @@ public class JsonUtils {
 
     public static String toJsonString(Object object) throws JsonProcessingException {
         ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+
         return objectMapper.writeValueAsString(object);
     }
 
