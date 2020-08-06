7c169fc13953abbfa4aa819d36965ccaa816e170
marcin007 <marcin007wojciechowski@gmail.com>
2020-08-06 19:20:46 +0200

    Code refactor

--- src/test/java/com/marcinwo/youtubeapi/demo/service/CategoryServiceTest.java
+++ src/test/java/com/marcinwo/youtubeapi/demo/service/CategoryServiceTest.java
@@ -1,6 +1,9 @@
 package com.marcinwo.youtubeapi.demo.service;
 
+import com.marcinwo.youtubeapi.demo.ExampleData;
+import com.marcinwo.youtubeapi.demo.dto.PatchCategoryDTO;
 import com.marcinwo.youtubeapi.demo.entity.Category;
+import com.marcinwo.youtubeapi.demo.exeption.CategoryNotFoundException;
 import com.marcinwo.youtubeapi.demo.repository.CategoryRepository;
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
@@ -14,13 +17,14 @@ import org.springframework.test.context.junit4.SpringRunner;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.*;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
-public class  CategoryServiceTest {
+public class CategoryServiceTest {
 
     @Autowired
     private CategoryService categoryService;
@@ -39,10 +43,73 @@ public class  CategoryServiceTest {
 
         List<Category> serviceCategories = categoryService.getCategories();
         assertThat(serviceCategories).isEqualTo(categories);
+
         verify(categoryRepository, times(1)).findAll();
         verifyNoMoreInteractions(categoryRepository);
     }
 
+    @Test
+    public void given_categoriesNotExist_when_getCategories_then_returnEmptyList() {
+        List<Category> categories = new ArrayList<>();
 
+        when(categoryRepository.findAll()).thenReturn(categories);
+
+        List<Category> serviceCategories = categoryService.getCategories();
+
+        assertThat(serviceCategories).isEmpty();
+    }
+
+    @Test
+    public void given_categoryExist_when_findCategoryById_then_returnCategory() {
+        Category category = new Category(1L, "comedy");
+
+        when(categoryRepository.findById(1L)).thenReturn(Optional.of(category));
+
+        Category categoryById = categoryService.findCategoryById(1L);
+
+        assertThat(categoryById).isEqualTo(category);
+    }
+
+    @Test(expected = CategoryNotFoundException.class)
+    public void given_categoryNotExist_when_findCategoryById_then_categoryNotFoundException() {
+        when(categoryRepository.findById(1L)).thenThrow(new CategoryNotFoundException("Category not found."));
+
+        categoryService.findCategoryById(1L);
+    }
+
+    @Test
+    public void given_categoryExist_when_postCategory_then_saveCategory() {
+        Category category = new Category(1L, "comedy");
+
+        when(categoryRepository.save(category)).thenReturn(category);
+
+        Category categorySaved = categoryService.postCategory(category);
+
+        assertThat(categorySaved).isNotNull();
+        verify(categoryRepository).save(any(Category.class));
+    }
+
+    @Test
+    public void given_categoryExist_when_deleteCategory_then_categoryNotExist() {
+
+        doNothing().when(categoryRepository).deleteById(1L);
+
+        categoryRepository.deleteById(1L);
+
+        verify(categoryRepository, times(1)).deleteById(1L);
+    }
+
+    @Test
+    public void given_categoryExist_when_updateCategoryById_then_categoryNameUpdated() {
+        Category category = new Category(1L, "comedy");
+        PatchCategoryDTO patchCategoryDTO = new PatchCategoryDTO("comedy updated");
+
+        when(categoryRepository.findById(1L)).thenReturn(Optional.of(category));
+        when(categoryRepository.save(category)).thenReturn(category);
+
+        Category updatedCategory = categoryService.updateCategoryById(1L, patchCategoryDTO);
+
+        assertThat(updatedCategory.getName()).isEqualTo("comedy updated");
+    }
 
 }
--- src/test/java/com/marcinwo/youtubeapi/demo/service/CommentServiceTest.java
+++ src/test/java/com/marcinwo/youtubeapi/demo/service/CommentServiceTest.java
@@ -2,7 +2,8 @@ package com.marcinwo.youtubeapi.demo.service;
 
 import com.marcinwo.youtubeapi.demo.ExampleData;
 import com.marcinwo.youtubeapi.demo.dto.PatchCommentDTO;
-import com.marcinwo.youtubeapi.demo.entity.Comment;
+import com.marcinwo.youtubeapi.demo.entity.*;
+import com.marcinwo.youtubeapi.demo.exeption.CommentNotFoundException;
 import com.marcinwo.youtubeapi.demo.repository.CommentRepository;
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
@@ -13,7 +14,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.time.LocalDateTime;
-import java.util.Optional;
+import java.util.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.*;
@@ -28,8 +29,108 @@ public class CommentServiceTest {
     @MockBean
     private CommentRepository commentRepository;
 
+
     @Test
-    public void given_commentExist_when_updateCommentById_then_OnlyContentAndUpdatedAtAreUpdated(){
+    public void given_commentsExist_when_getComments_then_returnCommentsList() {
+        List<Comment> commentList = ExampleData.comments();
+
+        when(commentRepository.findAll()).thenReturn(commentList);
+
+        List<Comment> comments = commentService.getComments();
+
+        assertThat(comments).isEqualTo(commentList);
+
+    }
+
+    @Test
+    public void given_commentsNotExist_when_getComments_then_returnEmptyCommentsList() {
+        List<Comment> commentList = new ArrayList<>();
+
+        when(commentRepository.findAll()).thenReturn(commentList);
+
+        List<Comment> comments = commentService.getComments();
+
+        assertThat(comments).isEmpty();
+    }
+
+    @Test
+    public void given_filmHasComments_when_getCommentsByFilmId_then_returnCommentList() {
+        Film film = new Film(
+                LocalDateTime.now(), "Matrix", "costam", "url", 120,
+                new Channel("channel1", "channel desc", new User(), new HashSet<>()), new HashSet<>(), new Category()
+        );
+        film.setId(1L);
+        List<Comment> commentList = List.of(
+                new Comment(
+                        film,
+                        new User(),
+                        LocalDateTime.now(),
+                        LocalDateTime.now(),
+                        "comment content1",
+                        11,
+                        22,
+                        Set.of(new Reply())),
+                new Comment(
+                        film,
+                        new User(),
+                        LocalDateTime.now(),
+                        LocalDateTime.now(),
+                        "comment content2",
+                        44,
+                        55,
+                        Set.of(new Reply())));
+
+
+        when(commentRepository.findAllByFilm_Id(1L)).thenReturn(commentList);
+
+        List<Comment> commentsByFilmId = commentService.getCommentsByFilmId(1L);
+
+        assertThat(commentsByFilmId).isEqualTo(commentList);
+    }
+
+    @Test
+    public void given_filmHasNoChannels_when_getCommentsByFilmId_then_returnEmptyChannelList() {
+        List<Comment> commentList = new ArrayList<>();
+
+        when(commentRepository.findAllByFilm_Id(1L)).thenReturn(commentList);
+
+        List<Comment> commentsByFilmId = commentService.getCommentsByFilmId(1L);
+
+        assertThat(commentsByFilmId).isEmpty();
+    }
+
+    @Test
+    public void given_commentExist_when_save_then_commentSaved(){
+        Comment comment = ExampleData.comment();
+
+        when(commentRepository.save(comment)).thenReturn(comment);
+
+        Comment commentSaved = commentService.save(comment);
+
+        assertThat(commentSaved).isEqualTo(comment);
+
+    }
+
+    @Test
+    public void given_commentExist_when_getCommentById_then_returnOptionalOfComment(){
+        Comment comment = ExampleData.comment();
+
+        when(commentRepository.findById(1L)).thenReturn(Optional.of(comment));
+
+        Comment commentById = commentService.getCommentById(1L);
+
+        assertThat(commentById).isEqualTo(comment);
+    }
+
+    @Test(expected = CommentNotFoundException.class)
+    public void given_commentNotExist_when_getCommentById_then_returnCommentNotExist(){
+        when(commentRepository.findById(1L)).thenThrow(new CommentNotFoundException("Comment not found"));
+
+        commentService.getCommentById(1L);
+    }
+
+    @Test//ok - simply behavioral test
+    public void given_commentExist_when_updateCommentById_then_OnlyContentAndUpdatedAtAreUpdated() {
 //        given
         Comment comment = mock(Comment.class);
 
@@ -40,16 +141,16 @@ public class CommentServiceTest {
         when(commentRepository.findById(1L)).thenReturn(Optional.of(comment));
         when(commentRepository.save(comment)).thenReturn(comment);
 
-        commentService.updateCommentById(1L, patchCommentDTO);
+        Comment commentUpdated = commentService.updateCommentById(1L, patchCommentDTO);
 
 //        then
         verify(comment, times(1)).setContent("ela");
         verify(comment, times(1)).setUpdatedAt(updatedAt);
-        verifyNoMoreInteractions(comment);
+        verifyNoMoreInteractions(commentUpdated);
     }
 
     @Test
-    public void given_commentExist_when_updateCommentById_then_ContentAndUpdatedAtAreUpdated(){
+    public void given_commentExist_when_updateCommentById_then_ContentAndUpdatedAtAreUpdated() {
 //        given
         Comment comment = ExampleData.comment2();
         comment.setId(1L);
@@ -63,10 +164,18 @@ public class CommentServiceTest {
         when(commentRepository.findById(1L)).thenReturn(Optional.of(comment));
         when(commentRepository.save(comment)).thenReturn(comment);
 
-        commentService.updateCommentById(1L, patchCommentDTO);
+        Comment commentUpdated = commentService.updateCommentById(1L, patchCommentDTO);
 
 //        then
-        assertThat(comment.getContent()).isEqualTo("ela");
-        assertThat(comment.getUpdatedAt()).isEqualTo(updatedAt);
+        assertThat(commentUpdated.getContent()).isEqualTo("ela");
+        assertThat(commentUpdated.getUpdatedAt()).isEqualTo(updatedAt);
     }
+
+    @Test//todo
+    public void given_commentExist_when_deleteCommentById_commentNotExist(){
+
+
+    }
+
+
 }
